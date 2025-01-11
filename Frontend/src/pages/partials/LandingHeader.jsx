bc56f6eb88cdbcff948e16ead894b4dc30216eef
Matthew <matthewgab24@gmail.com>
2025-01-11 23:44:47 +0800

    header added 2

--- Frontend/src/pages/partials/LandingHeader.jsx
+++ Frontend/src/pages/partials/LandingHeader.jsx
@@ -6,7 +6,7 @@ export default function LandingHeader() {
 	return (
 
 		<header
-			className='flex justify-between w-full h-10 border-b border-b-gray-600'
+			className='flex justify-between w-full h-10 border-b border-b-gray-600 px-40'
 		>
 			<div
 				className='flex justify-start items-center h-full w-auto'
@@ -36,16 +36,25 @@ export default function LandingHeader() {
 					<FontAwesomeIcon icon={faCaretDown} />
 				</button>
 				<a href='/'
-					className='flex justify-center items-center w-32 h-4/5 gap-2 font-medium text-white rounded-lg outline-none focus:outline-1 focus:outline-teal-400 hover:bg-gray-800'
+					className='flex justify-center items-center w-auto px-4 h-4/5 gap-2 font-medium text-white rounded-lg outline-none focus:outline-1 focus:outline-teal-400 hover:bg-gray-800'
 				>
 					Blog
 				</a>
 			</div>
 
 			<div
-				className='flex'
+				className='flex justify-start items-center h-full w-auto gap-2'
 			>
-
+				<a href='/'
+					className='flex justify-center items-center w-32 h-4/5 gap-2 font-medium text-white rounded-lg outline-none focus:outline-1 focus:outline-teal-400 hover:bg-gray-800'
+				>
+					Contact Us
+				</a>
+				<a href='/'
+					className='flex justify-center items-center w-32 h-4/5 gap-2 font-medium text-teal-400 border border-teal-400 outline-none focus:outline-1 focus:outline-teal-400 hover:bg-gray-800'
+				>
+					Try it now
+				</a>
 			</div>
 
 		</header>
