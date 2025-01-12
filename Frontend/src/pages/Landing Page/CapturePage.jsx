ccdddb93839687b3d2a93c824d5c9c5916785240
Matthew Cania <139668141+MattCania@users.noreply.github.com>
2025-01-12 13:24:50 +0800

    here we go with the scrolling mechanism again

--- Frontend/src/pages/Landing Page/CapturePage.jsx
+++ Frontend/src/pages/Landing Page/CapturePage.jsx
@@ -1,32 +1,24 @@
-
-export default function CapturePage () {
-
+export default function CapturePage() {
 	return (
 		<section
-			className='flex flex-col justify-start items-center h-screen w-full py-4 gap-4 overflow-x-hidden overflow-y-auto'
+			className="flex flex-col justify-start items-center h-screen w-full py-4 gap-4 overflow-x-hidden overflow-y-auto"
 		>
 			<div
-				className='flex flex-col justify-center items-center h-64 w-1/2 rounded-xl border border-teal-400 shadow-md shadow-teal-400'
+				className="flex flex-col justify-center items-center h-64 w-1/2 rounded-xl border border-teal-400 shadow-md shadow-teal-400"
 			>
-				<h1 
-					className='text-white font-medium text-4xl'
-				>
+				<h1 className="text-white font-medium text-4xl">
 					Welcome to GitFit!
 				</h1>
-				<p
-					className='text-xl text-white'
-				>
+				<p className="text-xl text-white">
 					Manage your career, goals, and health!
 				</p>
 			</div>
 
-			<div 
-				className='flex flex-col justify-center items-center h-[100vh] w-4/5 border-x-2 border-teal-400'
+			<div
+				className="flex flex-col justify-center items-center w-4/5 border-x-2 border-teal-400"
+				style={{ minHeight: "100%", height: 'auto' }}
 			>
-
-
 			</div>
-
 		</section>
-	)
+	);
 }
